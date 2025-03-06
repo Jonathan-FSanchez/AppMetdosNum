13edd85b234ef591e411ed1d669cad635fc8859c
Adan123D <1im10ortizmejiaadan@gmail.com>
2025-03-06 13:54:36 -0600

    Conexion con CSS

--- src/main/java/application/App.java
+++ src/main/java/application/App.java
@@ -1,14 +1,36 @@
 package application;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class App extends Application {
+
     public static void main(String[] args) {
+        launch(args);
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
+    public void start(Stage stage) {
+        try {
+            // Cargar el archivo FXML
+            Parent root = FXMLLoader.load(getClass().getResource("/views/menu.fxml"));
 
+            // Crear la escena
+            Scene scene = new Scene(root);
+
+            // Cargar el archivo CSS
+            scene.getStylesheets().add(getClass().getResource("/CSS/menu.css").toExternalForm());
+
+            // Configuración del Stage
+            stage.setScene(scene);
+            stage.setTitle("Aplicación JavaFX");
+            stage.show();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
